76e743ff308d0f2318acc52c6cc684482d87be5e
diegodurli <dgodurli@gmail.com>
2014-04-07 00:34:08 -0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LifeHospital::Application.config.secret_key_base = '9b7b679de15b6bdba29597f76e66be94bd01e76475e994fe54fda329846547701e08929fcb90a4dda7658d3442ed7d92187b54fd4d5eb204e850315019f92ddd'
